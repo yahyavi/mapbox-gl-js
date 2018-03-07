c43ab432187ce7b4561cd8cb0232ea4c009160b7
Molly Lloyd <molly@mapbox.com>
2018-03-06 16:54:50 -0800

    fixup

--- flow-typed/style-spec.js
+++ flow-typed/style-spec.js
@@ -187,7 +187,7 @@ declare type LineLayerSpecification = {|
         "line-offset"?: DataDrivenPropertyValueSpecification<number>,
         "line-blur"?: DataDrivenPropertyValueSpecification<number>,
         "line-dasharray"?: PropertyValueSpecification<Array<number>>,
-        "line-pattern"?: PropertyValueSpecification<string>,
+        "line-pattern"?: DataDrivenPropertyValueSpecification<string>,
         "line-gradient"?: ExpressionSpecification
     |}
 |}
--- src/data/bucket/line_attributes.js
+++ src/data/bucket/line_attributes.js
@@ -7,18 +7,17 @@ export const lineLayoutAttributes = createLayout([
 ], 4);
 
 // these attributes are only needed if using data-driven line-pattern
-// todo: pack these attributes tighter
 export const linePatternSourceExpressionLayout = createLayout([
-                                                        // [tl.x, tl.y, br.x, br.y]
-                                                        {name: 'a_pattern_a',  components: 2, type: 'Float32'},
-                                                        {name: 'a_pattern_b',  components: 2, type: 'Float32'}
-                                                        ]);
+    // [tl.x, tl.y, br.x, br.y]
+    {name: 'a_pattern_a',  components: 2, type: 'Float32'},
+    {name: 'a_pattern_b',  components: 2, type: 'Float32'}
+]);
 
 export const linePatternCompositeExpressionLayout = createLayout([
-                                                           // [tl.x, tl.y, br.x, br.y]
-                                                           {name: 'a_pattern_a',  components: 4, type: 'Float32'},
-                                                           {name: 'a_pattern_b',  components: 4, type: 'Float32'}
-                                                           ]);
+    // [tl.x, tl.y, br.x, br.y]
+    {name: 'a_pattern_a',  components: 4, type: 'Float32'},
+    {name: 'a_pattern_b',  components: 4, type: 'Float32'}
+]);
 
 
 export default lineLayoutAttributes;
